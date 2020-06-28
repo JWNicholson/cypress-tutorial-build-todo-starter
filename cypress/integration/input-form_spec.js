9c37d348f4b9de85c9a6fa1f6c88d210b396c24c
JW Nicholson <john.nicholson812@gmail.com>
2020-06-28 16:25:53 -0400

    set localhost:3030 as base url in cypress.json. Refactored input-form.spec to use variables for cy.visit and typedText

--- cypress/integration/input-form_spec.js
+++ cypress/integration/input-form_spec.js
@@ -1,5 +1,22 @@
 describe('Input form', () => {
-    it('focuses input on load', () => {
-        cy.visit('http://localhost:3030')
+
+    beforeEach(() => {
+        cy.visit('/')
     })
-})
+
+    it('focuses input on load', () => {
+       
+        cy.focused()
+            .should('have.class', 'new-todo')
+    })
+
+    it('accepts input', () => {
+        const typedText = 'Pickup Natalie'
+        
+
+        cy.get('.new-todo')
+        .type(typedText)
+        .should('have.value', typedText)
+    })
+
+})//describe Input form
