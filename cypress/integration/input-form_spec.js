60d436d0e77e13eca84b2ced3cf468bef977fc60
JW Nicholson <john.nicholson812@gmail.com>
2020-06-29 18:18:09 -0400

    completed form submission & XHR tests

--- cypress/integration/input-form_spec.js
+++ cypress/integration/input-form_spec.js
@@ -19,4 +19,47 @@ describe('Input form', () => {
         .should('have.value', typedText)
     })
 
+    context('Form submission', () => {
+        beforeEach(() => {
+            cy.server()
+        })
+        // "it.only" prefix lets test run by itself (without other tests running)
+        it('Adds a new todo when submitted', () => {
+            const itemText = 'Buy lemons'
+            cy.server()//lets us stub responses (meaning a backend is not neccessary for testing)
+            cy.route('POST', 'api/todos',{
+                name: itemText,
+                id: 1,
+                isComplete: false
+            })
+            cy.get('.new-todo')
+            .type(itemText)
+            .type('{enter}')
+            .should('have.value', '')
+
+           cy.get('.todo-list li')
+             .should('have.length', 1)
+             .and('contain', itemText) 
+        })
+
+        it('Shows error message on a failed submission', () => {
+      
+            cy.route({
+                url: '/api/todos',
+                method: 'POST',
+                status: 500,
+                response: {}
+            })
+
+            cy.get('.new-todo')
+                .type('test{enter}')
+
+            cy.get('.todo-list li')
+            .should('not.exist')
+
+            cy.get('.error')
+            .should('be.visible')
+        })
+    })
+
 })//describe Input form
